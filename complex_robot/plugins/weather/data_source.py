30bbd3defc7a277fa094f1be44023c0e26d241da
HXhlx <1367557521@qq.com>
2019-07-06 15:34:38 +0800

    添加新功能

--- complex_robot/plugins/weather/data_source.py
+++ complex_robot/plugins/weather/data_source.py
@@ -1,2 +1,11 @@
+from aiohttp import request
+
+
 async def get_weather_of_city(city: str) -> str:
-    return f'{city}的天气是……'
+    weather_type = ['now', 'forecast', 'hourly', 'lifestyle']
+    parameters = {'version': 'v6', 'city': city}
+    url = 'https://www.tianqiapi.com/api/?'
+    async with request('GET', url, params=parameters) as resp:
+        print(resp.url)
+        data = await resp.json()
+        return f"{city}的天气是{data.get('wea', '')},温度{data.get('tem', '')}℃,湿度{data.get('humidity', '')},能见度{data.get('visibility', '')}"
---
+++ complex_robot/plugins/zhihu.py
@@ -0,0 +1,19 @@
+from nonebot import on_command, CommandSession
+import aiohttp
+
+
+@on_command('知乎日报')
+async def news(session:CommandSession):
+    STORY_URL_FORMAT = 'https://daily.zhihu.com/story/{}'
+    async with aiohttp.request('GET', 'https://news-at.zhihu.com/api/4/news/latest', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}) as resp:
+        data = await resp.json()
+        stories = data.get('stories')
+        if not stories:
+            await session.send('暂时没有数据, 或者服务无法访问')
+            return
+        reply = ''
+        for story in stories:
+            url = STORY_URL_FORMAT.format(story['id'])
+            title = story.get('title', '未知内容')
+            reply += f'\n{title}\n{url}\n'
+        await session.send(reply)
